78bedf828a4579890d873f11850b1109a6efaa7d
mizikkhan <zaedkhan2019@gmail.com>
2023-07-13 21:24:15 +0600

    Added Artwork.sol

--- contracts/Artwork.sol
+++ contracts/Artwork.sol
@@ -9,13 +9,24 @@ contract Artwork {
         uint256 price;
         string credentials;
         uint256 quantity;
+    }
+
+    struct Purchase {
+        uint256 id;
+        uint256 artId;
+        address buyer;
+        address seller;
+        uint256 amount;
         bool delivery_state;
     }
 
     uint256 art_count;
+    uint256 purchase_count;
 
     mapping(uint256 => Art) public artworks;
+    mapping(uint256 => Purchase) public purchases;
 
+    // Create Artwork
     function createArt(
         address _owner,
         string memory _image_path,
@@ -31,10 +42,10 @@ contract Artwork {
         art.price = _price;
         art.credentials = _credentials;
         art.quantity = _quantity;
-        art.delivery_state = false;
         art_count++;
     }
 
+    // Update Product Quantity
     function updateQuantity(
         uint256 art_id,
         uint256 _new_quantity
@@ -44,25 +55,86 @@ contract Artwork {
         return art.quantity;
     }
 
-    function startDelivery(uint256 art_id) public returns (bool) {
-        Art storage art = artworks[art_id];
-        art.delivery_state = true;
-        return art.delivery_state;
+    // Complete delivery
+    function startDelivery(uint256 purchase_id) public returns (bool) {
+        Purchase storage purchase = purchases[purchase_id];
+        purchase.delivery_state = true;
+        return purchase.delivery_state;
     }
 
-    function getDeliveryStatus(uint256 art_id) public view returns (bool) {
-        Art storage art = artworks[art_id];
-        return art.delivery_state;
-    }
+    // To be continued...
+    // function getDeliveryStatus(uint256 art_id) public view returns (bool) {
+    //     Art storage art = artworks[art_id];
+    //     return art.delivery_state;
+    // }
 
+    // To be continued...
     function getArtQuantity(uint256 art_id) public view returns (uint256) {
         Art storage art = artworks[art_id];
         return art.quantity;
     }
 
-    function cancelDelivery(uint256 art_id) public returns (bool) {
+    // To be continued...
+    // function cancelDelivery(uint256 art_id) public returns (bool) {
+    //     Art storage art = artworks[art_id];
+    //     art.delivery_state = false;
+    //     return art.delivery_state;
+    // }
+
+    // Get All Artworks
+    function getArtworks() public view returns (Art[] memory) {
+        Art[] memory allArtworks = new Art[](art_count);
+
+        for (uint i = 0; i < art_count; i++) {
+            Art storage item = artworks[i];
+
+            allArtworks[i] = item;
+        }
+
+        return allArtworks;
+    }
+
+    // Purchase Event
+    event ArtworkPurchased(
+        uint256 artId,
+        address buyer,
+        address seller,
+        uint256 amount
+    );
+
+    // Purchase Artwork and add order to purchases
+    function buyArtwork(uint256 art_id) public payable {
         Art storage art = artworks[art_id];
-        art.delivery_state = false;
-        return art.delivery_state;
+
+        require(art.quantity > 0, "Artwork is out of stock");
+
+        uint256 amount = art.price;
+
+        require(msg.value == amount, "Incorrect payment amount");
+
+        (bool sent, ) = payable(art.owner).call{value: amount}("");
+        require(sent, "Payment failed");
+
+        art.quantity--;
+
+        Purchase storage purchase = purchases[purchase_count];
+        purchase.id = purchase_count;
+        purchase.artId = art_id;
+        purchase.buyer = msg.sender;
+        purchase.seller = art.owner;
+        purchase.amount = amount;
+        purchase.delivery_state = false;
+        purchase_count++;
+        emit ArtworkPurchased(art_id, msg.sender, art.owner, amount);
+    }
+
+    // Get All Purchases
+    function getPurchases() public view returns (Purchase[] memory) {
+        Purchase[] memory allPurchases = new Purchase[](purchase_count);
+        for (uint i = 0; i < purchase_count; i++) {
+            Purchase storage item = purchases[i];
+            allPurchases[i] = item;
+        }
+        return allPurchases;
     }
 }
